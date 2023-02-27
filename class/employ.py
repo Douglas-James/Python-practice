f21c5c4ef8c6aa4b6d67794cd89433d090d8daf9
Douglas-James <ryle@drumclog.com>
2023-02-27 16:39:05 -0600

    'update on salary'

--- class/employ.py
+++ class/employ.py
@@ -13,7 +13,10 @@ class Department():
     if not self.employees:
       print(f"No employeees in {self.name}")
     for employee in self.employees:
-      print(f"{employee.fname[0]}. {employee.lname[0:3]} - Salary: {employee.sal}")
+      if employee.sal >= 2000000: return print(f" Can't make it more than This salary ${employee.sal}")
+      elif  employee.sal <= 100000: return print(f"{employee.fname[0]}. {employee.lname[0:3]} - Salary: {employee.sal + 6000}")
+      else: return print(f"something went wrong salary: {employee.fname[0]}. {employee.lname[0:3]} - Salary: {employee.sal}")
+      # print(f"{employee.fname[0]}. {employee.lname[0:3]} - Salary: {employee.sal}")
 
   # _str_
   def __str__(self):
@@ -54,7 +57,7 @@ department_4 = Department("Legal")
 departments = [department_1, department_2, department_3, department_4]
 
 # create an instance of employ
-new_employee = Employ("James", "Douglas", 200000, department_2)
+new_employee = Employ("James", "Douglas", 200000000, department_2)
 new_employee1 = Employ("Bob", "Squal", 32000, department_1)
 new_employee2 = Employ("Squick", "Scream", 89000, department_3)
 new_employee = Employ("R", "Douglas", 81000, department_1)
@@ -64,8 +67,8 @@ employees_list = [new_employee, new_employee1, new_employee2]
 
 
 # Printing  employee
-for employees in employees_list:  
-  print(f"Full Name {employees.full_name()} : Salary ${employees.rise_salary()}")
+# for employees in employees_list:  
+#   print(f"Full Name {employees.full_name()} : Salary ${employees.rise_salary()}")
 
 
 # Printing Department
