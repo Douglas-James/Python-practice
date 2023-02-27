dde12786b68646e4c7c5a5bad6c9a9510992d99c
Douglas-James <ryle@drumclog.com>
2023-02-27 15:00:28 -0600

    'updated singly Linked list'

--- singlyLinkedlist/singlLinkedlist.py
+++ singlyLinkedlist/singlLinkedlist.py
@@ -75,7 +75,66 @@ class SinglyLinkedList:
      self.length += 1
      return self
 
+  # Reverse the -infinity - +infinity order to a - z
+  def reverse(self):
+
+    arr = []
+    # node  current head 
+    node = self.head
+
+    # head equal to the tail
+    self.head = self.tail
+
+    #let next be None
+    next = None
+
+    # prev equal to None
+    prev = None
+    
+
+    # length of length
+
+
+    for i in range(0, self.length):
+      arr.append(node.val)
+      next = node.next
+      node.next = prev
+
+      prev = node
+      node = next
+    
+    return arr
   
+  # get would get the val of index you have requested
+  def get(self, index):
+    # index shouldn't access below zero or greater then the length 
+    if index < 0 or index >= self.length: return None
+    # count becuse we are going to use while loop
+    count = 0
+
+    # currenthead would equal to the head
+    currenthead = self.head
+
+    # while loop is not we equal and is not index
+    while count != index:
+      # currenthead is equal to next head
+      currenthead = currenthead.next
+      # count incremented by one
+      count += 1
+    
+    # return currenthead val equal value
+    return currenthead.val
+
+  # remove use the index to remove the val
+  # def remove(self, index):
+  #   if index < 0 or index >= self.length: return None 
+  #   if index == 0: return self.shift()
+  #   if index == self.length - 1: return self.pop()
+    
+
+  #   self.length -= 1
+  #   return self
+
 
   # print list method show me in details
   def printlist(self):
@@ -94,9 +153,13 @@ class SinglyLinkedList:
       arr.append(currentnode.val)
       currentnode = currentnode.next
   
-   n = len(arr)
-   for i in range(n):
-       for j in range(0, n-i-1):
+   length = len(arr)
+
+  #  print(length) # it just check the length of the arr
+
+   # here we could use self.length 
+   for i in range(length):
+       for j in range(0, length-i-1):
           if arr[j] > arr[j+1] :
               arr[j], arr[j+1] = arr[j+1], arr[j]      
    return arr
@@ -111,6 +174,8 @@ singly.push(20)
 singly.push(30)
 singly.push(-1)
 singly.push(-2)
+singly.push(3)
+singly.push(1000)
 
 # pop method remove from the last
 singly.pop()
@@ -124,12 +189,22 @@ singly.shift()
 singly.unshift(50)
 singly.unshift(300)
 
-# sort 
+# get method should get index of val
+print(singly.get(2))
+
+# remove 
+# singly.remove(2)
+
+# sort should sort the array passed upon the val
 print(singly.sort())
 
 
+# reverse order
+print(singly.reverse())
+
+
 # print method for me
-print(singly.printlist())
+# print(singly.printlist())
 
 
   
